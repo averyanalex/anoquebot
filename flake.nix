dfc5dd1d58c1ebdaeec5e1d6191af6b63d02cf80
AveryanAlex <alex@averyan.ru>
2024-01-28 13:59:43 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,100 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    import-cargo.url = "github:edolstra/import-cargo";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    import-cargo,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+        inherit (import-cargo.builders) importCargo;
+
+        rust = pkgs.rust-bin.stable.latest.default;
+
+        devInputs =
+          (with pkgs; [
+            alejandra
+            black
+            poetry
+            sqlx-cli
+            postgresql
+          ])
+          ++ [
+            pgstart
+            pgstop
+          ];
+        buildInputs = [pkgs.openssl];
+        nativeBuildInputs = [rust pkgs.pkg-config];
+
+        package = pkgs.stdenv.mkDerivation {
+          name = "anoquebot";
+          src = self;
+
+          inherit buildInputs;
+
+          nativeBuildInputs =
+            nativeBuildInputs
+            ++ [
+              (importCargo {
+                lockFile = ./Cargo.lock;
+                inherit pkgs;
+              })
+              .cargoHome
+            ];
+
+          buildPhase = ''
+            cargo build --release --offline
+          '';
+
+          installPhase = ''
+            install -Dm775 ./target/release/anoquebot $out/bin/anoquebot
+          '';
+        };
+
+        pgstart = pkgs.writeShellScriptBin "pgstart" ''
+          if [ ! -d $PGHOST ]; then
+            mkdir -p $PGHOST
+          fi
+          if [ ! -d $PGDATA ]; then
+            echo 'Initializing postgresql database...'
+            LC_ALL=C.utf8 initdb $PGDATA --auth=trust >/dev/null
+          fi
+          OLD_PGDATABASE=$PGDATABASE
+          export PGDATABASE=postgres
+          pg_ctl start -l $LOG_PATH -o "-c listen_addresses= -c unix_socket_directories=$PGHOST"
+          psql -tAc "SELECT 1 FROM pg_database WHERE datname = 'anoquebot'" | grep -q 1 || psql -tAc 'CREATE DATABASE "anoquebot"'
+          export PGDATABASE=$OLD_PGDATABASE
+        '';
+
+        pgstop = pkgs.writeShellScriptBin "pgstop" ''
+          pg_ctl -D $PGDATA stop | true
+        '';
+      in {
+        packages.default = package;
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = devInputs ++ buildInputs ++ nativeBuildInputs;
+
+            shellHook = ''
+              export PGDATA=$PWD/postgres/data
+              export PGHOST=$PWD/postgres
+              export LOG_PATH=$PWD/postgres/LOG
+              export PGDATABASE=anoquebot
+              export DATABASE_URL=postgresql:///anoquebot?host=$PWD/postgres;
+            '';
+          };
+        };
+      }
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,219 @@
+use std::{collections::BTreeMap, future::Future, str::FromStr};
+
+use anyhow::Result;
+use sentry::protocol::Value;
+use teloxide::{
+    adaptors::{throttle::Limits, Throttle},
+    dispatching::dialogue::InMemStorage,
+    macros::BotCommands,
+    prelude::*,
+    types::Chat,
+};
+use tracing::*;
+use tracing_subscriber::prelude::*;
+
+type Bot = Throttle<teloxide::Bot>;
+
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    Start,
+    WaitMessage {
+        chat: i64,
+    },
+}
+
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+
+#[derive(Debug, BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "Доступные команды:")]
+enum Command {
+    #[command(rename = "start")]
+    SendQuestion { chat: i64 },
+    #[command(rename = "start")]
+    Start,
+}
+
+fn main() -> Result<()> {
+    std::env::set_var("RUST_BACKTRACE", "1");
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer().with_filter(
+                tracing_subscriber::filter::LevelFilter::from_str(
+                    &std::env::var("RUST_LOG").unwrap_or_else(|_| String::from("info")),
+                )
+                .unwrap_or(tracing_subscriber::filter::LevelFilter::INFO),
+            ),
+        )
+        .with(
+            sentry_tracing::layer().event_filter(|md| match *md.level() {
+                Level::TRACE => sentry_tracing::EventFilter::Ignore,
+                _ => sentry_tracing::EventFilter::Breadcrumb,
+            }),
+        )
+        .try_init()
+        .unwrap();
+
+    let _sentry_guard = match std::env::var("SENTRY_DSN") {
+        Ok(d) => {
+            let guard = sentry::init((
+                d,
+                sentry::ClientOptions {
+                    release: sentry::release_name!(),
+                    attach_stacktrace: true,
+                    traces_sample_rate: 1.0,
+                    ..Default::default()
+                },
+            ));
+            Some(guard)
+        }
+        Err(e) => {
+            warn!("can't get SENTRY_DSN: {:?}", e);
+            None
+        }
+    };
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()?
+        .block_on(_main())
+}
+
+async fn _main() -> Result<()> {
+    tracing::info!("Starting bot...");
+    let bot = teloxide::Bot::from_env().throttle(Limits::default());
+
+    let handler = Update::filter_message()
+        .enter_dialogue::<Message, InMemStorage<State>, State>()
+        .branch(
+            dptree::entry()
+                .filter_command::<Command>()
+                .endpoint(answer_cmd),
+        )
+        .branch(dptree::case![State::WaitMessage { chat }].endpoint(send_anon_message))
+        .branch(dptree::endpoint(invalid_command));
+
+    Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![InMemStorage::<State>::new()])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+
+    Ok(())
+}
+
+async fn try_handle(chat: &Chat, handle: impl Future<Output = Result<()>>) -> Result<()> {
+    sentry::start_session();
+    sentry::configure_scope(|scope| {
+        let mut map = BTreeMap::new();
+        if let Some(first_name) = chat.first_name() {
+            map.insert("first_name".to_owned(), Value::from(first_name));
+        }
+        if let Some(last_name) = chat.last_name() {
+            map.insert("last_name".to_owned(), Value::from(last_name));
+        }
+        scope.set_user(Some(sentry::User {
+            id: Some(chat.id.0.to_string()),
+            username: chat.username().map(|u| u.to_owned()),
+            other: map,
+            ..Default::default()
+        }));
+    });
+
+    if let Err(e) = handle.await {
+        sentry_anyhow::capture_anyhow(&e);
+    }
+
+    sentry::end_session();
+
+    Ok(())
+}
+
+async fn answer_cmd(bot: Bot, msg: Message, cmd: Command, dialogue: MyDialogue) -> Result<()> {
+    try_handle(&msg.chat, async move {
+        match cmd {
+            Command::Start => {
+                bot.send_message(
+                    msg.chat.id,
+                    format!(
+                        "Ваша персональная ссылка: https://t.me/anoquebot?start={}. Отправьте \
+                        её друзьям, чтобы начать получать анонимные вопросы!",
+                        msg.chat.id.0
+                    ),
+                )
+                .await?;
+                dialogue.update(State::Start).await?;
+            }
+            Command::SendQuestion { chat } => {
+                bot.send_message(msg.chat.id, "Напишите анонимный вопрос:")
+                    .await?;
+                dialogue.update(State::WaitMessage { chat }).await?;
+            }
+        }
+        Ok(())
+    })
+    .await
+}
+
+async fn send_anon_message(bot: Bot, msg: Message, dialogue: MyDialogue, chat: i64) -> Result<()> {
+    try_handle(&msg.chat.clone(), async move {
+        let Some(text) = msg.text() else {
+            bot.send_message(
+                msg.chat.id,
+                "В вашем сообщении нет текста! \
+            Изображения на данный момент не поддерживаются. Попробуйте ещё раз.",
+            )
+            .await?;
+            return Ok(());
+        };
+
+        if let Err(e) = bot
+            .send_message(
+                ChatId(chat),
+                format!("Вы получили анонимное сообщение:\n\n{text}"),
+            )
+            .await
+        {
+            bot.send_message(
+                msg.chat.id,
+                format!(
+                    "Не удалось отправить сообщение: {e}. \
+            Возможно, получатель заблокировал бота."
+                ),
+            )
+            .await?;
+            sentry::capture_error(&e);
+        } else {
+            bot.send_message(
+                msg.chat.id,
+                format!(
+                    "Ваше сообщение успешно отправлено!\n\n\
+            Ваша персональная ссылка: https://t.me/anoquebot?start={}. Отправьте \
+            её друзьям, чтобы начать получать анонимные вопросы!",
+                    msg.chat.id
+                ),
+            )
+            .await?;
+        }
+
+        dialogue.update(State::Start).await?;
+
+        Ok(())
+    })
+    .await
+}
+
+async fn invalid_command(bot: Bot, msg: Message) -> Result<()> {
+    try_handle(&msg.chat, async move {
+        bot.send_message(
+            msg.chat.id,
+            "Используйте /start, чтобы получить персональную \
+        ссылку, или перейдите по чей-то ссылке, чтобы отправить автору вопрос.",
+        )
+        .await?;
+        Ok(())
+    })
+    .await
+}
